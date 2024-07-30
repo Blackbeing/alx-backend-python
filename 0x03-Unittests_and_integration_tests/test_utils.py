576103d4106d4e05fade85923eb6e9fd8d049d09
Blackbeing <jonnieey55@gmail.com>
2024-07-30 19:18:43 +0300

    feat Mock url objects

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,7 +4,8 @@
 import unittest
 from parameterized import parameterized
 from typing import Dict, Union, Tuple
-from utils import access_nested_map
+from utils import access_nested_map, get_json
+from unittest.mock import Mock, patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -30,6 +31,21 @@ class TestAccessNestedMap(unittest.TestCase):
         """Use parameterized testing to test nested map"""
         with self.assertRaises(exception):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    def test_get_json(self, test_url: str, test_payload: Dict):
+        mock_obj = Mock()
+        mock_obj.json.return_value = test_payload
+        with patch("requests.get", return_value=mock_obj) as req:
+            self.assertEqual(get_json(test_url), test_payload)
+            req.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
