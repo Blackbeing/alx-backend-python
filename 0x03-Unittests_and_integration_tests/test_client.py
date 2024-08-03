7b28c82f43243f15ff006f89325873f9a057ac44
Blackbeing <jonnieey55@gmail.com>
2024-08-03 09:09:01 +0300

    fix Corrections

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,21 +3,18 @@
 
 import unittest
 from parameterized import parameterized
-from unittest.mock import patch, Mock
+from unittest.mock import patch
 from client import GithubOrgClient
-
-ORG_URL = "https://api.github.com/orgs/{org}"
 
 
 class TestGithubOrgClient(unittest.TestCase):
-    @parameterized.expand(
-        [
-            ("google", {"login": "google"}),
-            ("abc", {"login": "abc"}),
-        ]
-    )
+    """
+    Class to test github org client
+    """
+
+    @parameterized.expand([("google"), ("abc")])
     @patch("client.get_json")
-    def test_org(self, org, response, mock_get_json):
+    def test_org(self, input, mock_get_json):
         """
         Test case to validate the GithubOrgClient class for fetching
         organization data.
@@ -29,11 +26,7 @@ class TestGithubOrgClient(unittest.TestCase):
         Returns:
         - None
         """
-        mock_get_json.return_value = Mock(return_value=response)
-        gh_org = GithubOrgClient(org)
-        self.assertEqual(gh_org.org(), response)
-        mock_get_json.assert_called_once_with(ORG_URL.format(org=org))
-
-
-if __name__ == "__main__":
-    unittest.main()
+        gh_org = GithubOrgClient(input)
+        gh_org.org()
+        ORG_URL = "https://api.github.com/orgs/{org}"
+        mock_get_json.assert_called_once_with(ORG_URL.format(org=input))
